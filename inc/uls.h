f7c2b6e0b3c049542fbf456ed0f18c87426a0bd2
Volodymyr Aivazovskyi <aivazovskyi.volodymyr@gmail.com>
2020-10-04 19:24:16 +0300

    upd

--- inc/uls.h
+++ inc/uls.h
@@ -1,7 +1,14 @@
 #pragma once
 
 #include "libmx.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <pwd.h>
+#include <time.h>
+
+
 #include <dirent.h> // for opendir, readdir, closedir
+#include <stdio.h> // DELETE IT !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 
 
--- src/main/main.c
+++ src/main/main.c
@@ -1,21 +1,20 @@
 #include "uls.h"
 
-static void test(int argc, char **argv);
+static void tmain(int argc, char **argv);
 
 int main(int argc, char **argv) {
-    test(argc, argv);
+    tmain(argc, argv);
 
 //    system("leaks -q uls");
 }
 
 
-static void test(int argc, char **argv) {
+static void tmain(int argc, char **argv) {
     t_uls *uls = (t_uls *)malloc(sizeof(t_uls));
 
-
+ 
 
 
 
     free(uls);
-
 }
